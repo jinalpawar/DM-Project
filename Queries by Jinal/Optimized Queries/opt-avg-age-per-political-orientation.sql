b93079bd8a183c99281ffd545cd6c09a0d9ef126
Jinal Pawar <jinalpawar818@gmail.com>
2026-04-26 11:28:26 +0200

    Add .txt file for submission

---
+++ Queries by Jinal/Optimized Queries/opt-avg-age-per-political-orientation.sql
@@ -0,0 +1,19 @@
+---Create Index on User Political Orientations for better performance
+CREATE INDEX idx_user_po_user_id ON user_political_orientations(user_id);
+
+SELECT 
+	po.name as "Political Orientation", 
+	ROUND(AVG(u.age)) AS "Average User Age",
+	COUNT(DISTINCT(upo.user_id)) AS "User Count"
+FROM 
+	users AS u
+	JOIN user_political_orientations AS upo ON 
+		upo.user_id = u.user_id
+	JOIN political_orientations AS po ON 
+		upo.political_orientation_id = po.political_orientation_id
+GROUP BY 
+	po.name
+HAVING 
+	COUNT(DISTINCT(upo.user_id)) > 50
+ORDER BY 
+	"Average User Age"
